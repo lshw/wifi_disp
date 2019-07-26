a706bb161e792ee023f75e6d2e770975127b45bf
liu shiwei <liushiwei@gmail.com>
2019-07-26 13:31:58 +0800

    http请求2秒超时

--- wifi_disp/wifi_client.h
+++ wifi_disp/wifi_client.h
@@ -236,9 +236,8 @@ uint16_t http_get(uint8_t no) {
 
   Serial.println( url0); //串口输出
   http.begin( url0 ); //HTTP提交
-
+  http.setTimeout(2000);
   int httpCode;
-
   for (uint8_t i = 0; i < 10; i++) {
     httpCode = http.GET();
     if (httpCode < 0) {
