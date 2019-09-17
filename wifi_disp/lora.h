1393cdc3d25cc613cab03e8bdaa3edc47cf529c2
liu shiwei <liushiwei@gmail.com>
2019-09-18 00:19:34 +0800

    lora速率降为7.8k

--- wifi_disp/lora.h
+++ wifi_disp/lora.h
@@ -7,8 +7,6 @@ uint8_t len;
 uint8_t rxBuf[256];
 uint32_t send_delay = 0;
 uint16_t lora_count = 0;
-void set_1278() {
-}
 
 void lora_send_loop() {
   if (millis() - send_delay < 200) return;
@@ -41,13 +39,12 @@ void lora_receive_loop() {
 bool lora_state = false;
 void lora_init() {
   if (lora_state) return;
-  lora_state = lora.init(2, 1);
-  /*
+  lora_state = lora.init(2,1);
+  lora.sleep();    // turn to standby mode
     lora.setFrequency(434E6); //434Mhz
     lora.setRFpara(LR_BW_7p8k,LR_CODINGRATE_2,12,LR_PAYLOAD_CRC_ON); //BW带宽,CR编码率,SF扩频因子，CRC
     // preamble length is 6~65535
     lora.setPreambleLen(12);
     // mode LR_IMPLICIT_HEADER_MODE or LR_EXPLICIT_HEADER_MODE
     lora.setHeaderMode(LR_EXPLICIT_HEADER_MODE);
-  */
 }
