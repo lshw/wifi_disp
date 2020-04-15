777c5646c520244063c22e2b57f72c19c073c8d9
liu shiwei <liushiwei@gmail.com>
2020-04-15 16:39:36 +0800

    替换网址，清理老的服务器地址 bin文件 V1.46

--- wifi_disp/default.h
+++ wifi_disp/default.h
@@ -1,3 +1,3 @@
 
 #define DEFAULT_URL0 "http://temp.cfido.com:808/wifi_disp.php"
-#define DEFAULT_URL1 "http://temp2.wf163.com:88/wifi_disp.php"
+#define DEFAULT_URL1 "http://temp2.wf163.com:808/wifi_disp.php"
--- wifi_disp/wifi_disp.ino
+++ wifi_disp/wifi_disp.ino
@@ -1,5 +1,5 @@
 #include <FS.h>
-#define VER "1.45"
+#define VER "1.46"
 #define HOSTNAME "disp_"
 extern "C" {
 #include "user_interface.h"
