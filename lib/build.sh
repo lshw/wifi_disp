8fbd46f456978e64e7acc364e52c8016a0e92769
liu shiwei <liushiwei@gmail.com>
2021-04-05 22:41:28 +0800

    arduino升级1.8.13

--- lib/build.sh
+++ lib/build.sh
@@ -12,12 +12,15 @@ echo $ver
 export COMMIT=$ver
 #echo "#define GIT_COMMIT_ID \"$ver\"" > wifi_disp/commit.h
 
-arduino=/opt/arduino-1.8.12
+arduino=/opt/arduino-1.8.13
 arduinoset=~/.arduino15
 sketchbook=~/sketchbook
 mkdir -p /tmp/build_wifi_disp /tmp/cache_wifi_disp
 chown liushiwei /tmp/build_wifi_disp /tmp/cache_wifi_disp
-$arduino/arduino-builder -dump-prefs -logger=machine \
+
+$arduino/arduino-builder \
+-dump-prefs \
+-logger=machine \
 -hardware $arduino/hardware \
 -hardware $arduinoset/packages \
 -tools $arduino/tools-builder \
@@ -25,8 +28,8 @@ $arduino/arduino-builder -dump-prefs -logger=machine \
 -tools $arduinoset/packages \
 -built-in-libraries $arduino/libraries \
 -libraries $sketchbook/libraries \
--fqbn=esp8266com:esp8266:espduino:ResetMethod=v2,xtal=80,vt=flash,exception=legacy,eesz=4M3M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 \
--ide-version=10812 \
+-fqbn=esp8266com:esp8266:espduino:ResetMethod=v2,xtal=160,vt=flash,exception=disabled,eesz=4M3M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 \
+-ide-version=10813 \
 -build-path /tmp/build_wifi_disp \
 -warnings=none \
 -build-cache /tmp/cache_wifi_disp \
@@ -44,23 +47,19 @@ $arduino/arduino-builder \
 -tools $arduinoset/packages \
 -built-in-libraries $arduino/libraries \
 -libraries $sketchbook/libraries \
--fqbn=esp8266com:esp8266:espduino:ResetMethod=v2,xtal=80,vt=flash,exception=legacy,eesz=4M3M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 \
--ide-version=10812 \
+-fqbn=esp8266com:esp8266:espduino:ResetMethod=v2,xtal=160,vt=flash,exception=disabled,eesz=4M3M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=115200 \
+-ide-version=10813 \
 -build-path /tmp/build_wifi_disp \
 -warnings=none \
 -build-cache /tmp/cache_wifi_disp \
 -prefs=build.warn_data_percentage=75 \
 -verbose \
-./wifi_disp/wifi_disp.ino \
-> /tmp/info_wifi_disp.log
-if [ $? = 0 ] ; then
-#/opt/arduino-1.8.12/hardware/esp8266com/esp8266/tools/xtensa-lx106-elf/bin/xtensa-lx106-elf-size /tmp/build_wifi_disp/wifi_disp.ino.elf |head -n 5
- grep "Global vari" /tmp/info_wifi_disp.log |awk -F[ '{printf $2}'|tr -d ']'|awk -F' ' '{print "内存：使用"$1"字节,"$3"%,剩余:"$4"字节"}'
- grep "Sketch uses" /tmp/info_wifi_disp.log |awk -F[ '{printf $2}'|tr -d ']'|awk -F' ' '{print "ROM：使用"$1"字节,"$3"%"}'
+./wifi_disp/wifi_disp.ino
 
- cp -a /tmp/build_wifi_disp/wifi_disp.ino.bin lib/wifi_disp.bin
- lib/uncrc32 lib/wifi_disp.bin 0
- if [ "a$1" != "a"  ] ;then
-  $arduino/hardware/esp8266com/esp8266/tools/espota.py -p 8266 -i $1 -f lib/wifi_disp.bin
- fi
+$arduino/hardware/esp8266com/esp8266/tools/xtensa-lx106-elf/bin/xtensa-lx106-elf-size /tmp/build_wifi_disp/wifi_disp.ino.elf
+
+cp -a /tmp/build_wifi_disp/wifi_disp.ino.bin lib/wifi_disp.bin
+lib/uncrc32 lib/wifi_disp.bin 0
+if [ "a$1" != "a"  ] ;then
+ $arduino/hardware/esp8266com/esp8266/tools/espota.py -p 8266 -i $1 -f lib/wifi_disp.bin
 fi
