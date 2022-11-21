826cb1c60be3e7f08ebadb9153b3231ffda5a628
liu shiwei <liushiwei@gmail.com>
2022-11-21 17:10:38 +0800

    频道无效时，不做设置

--- wifi_disp/nvram.h
+++ wifi_disp/nvram.h
@@ -22,7 +22,7 @@ void load_nvram() {
   ESP.rtcUserMemoryRead(0, (uint32_t*) &nvram, sizeof(nvram));
   if (nvram.crc32 != calculateCRC32((uint8_t*) &nvram, sizeof(nvram) - sizeof(nvram.crc32))) {
     memset(&nvram, 0, sizeof(nvram));
-  } else {
+  } else if (nvram.ch > 0 && nvram.ch <= 14) {
     Serial.println("\r\nwifi channel=" + String(nvram.ch) + ", proc=" + String(nvram.proc));
     WRITE_PERI_REG(0x600011f4, 1 << 16 | nvram.ch);
   }
