29cbfe99c87ebd1d2c292aa60f36c6db520b4d16
liu shiwei <liushiwei@gmail.com>
2023-07-02 16:24:03 +0800

    服务器端演示程序，不再显示新冠感染数，换成显示汇率

--- lib/wifi_disp.php
+++ lib/wifi_disp.php
@@ -1,38 +1,55 @@
 <?php
-/*程序考虑了api数据缓存，尽快断开客户端链接，如果不需要这些，只需要一行代码就行了
+
+/*最基本的,只需要3行代码就行了,显示当前温度，然后数据存到 /tmp/data.csv
 echo "$_GET[temp],720";
-或者
-echo api("https://lab.ahusmart.com//nCoV/api/overall","confirmedCount").",3600"; //10小时后再上线
-*/
+$t = date('Y-m-d H:i:s'); //2023-07-02 15:52:00
+file_put_contents("/tmp/data.csv", "$t,$_GET[temp],$_GET[shidu]%\r\n", FILE_APPEND);
+ */
 
 ignore_user_abort(true);//客户端断开链接后继续执行php程序。
 ob_start();
-switch($_GET['type']) {
-	case 'wh':
-		echo api("https://lab.ahusmart.com//nCoV/api/overall","confirmedCount").",3600"; //10小时后再上线
-		break;
-	default:
-		echo "$_GET[temp],720"; //显示温度，2小时(720*10s)后再上线。
-		break;
+switch ($_GET['type']) {
+    case 'loadavg': //服务器端的平均负载
+        $loadavg = file_get_contents("/proc/loadavg");
+        $loadavg = explode(' ', file_get_contents('/proc/loadavg'));
+        echo "$loadavg[0],360";//探头显示loadavg,并且1小时后再次上线
+        break;
+    case 'USD_CNY':
+        $hl = api("https://open.er-api.com/v6/latest/USD", "rates", "CNY");
+        $hl = round($hl, 3);
+        echo "$hl,360";
+        break;
+    default:
+        echo "$_GET[temp],720"; //显示温度，2小时(720*10s)后再上线。
+        break;
 }
 $size=ob_get_length();
 header('Connection: close');//不需要持续链接
 header("Content-Length: $size");  //明确告诉浏览器数据长度,浏览器接收到此长度数据后就不再接收数据
 ob_end_flush();
-//save_data();//断开链接后，再保存数据，防止长时间挂住客户端，造成客户端费电。
+//断开链接后，再保存数据，让探头尽快休眠，节省探头电量。
+$t=date('Y-m-d H:i:s'); //2023-07-02 15:52:00
+file_put_contents("/tmp/data.csv", "$t,$_GET[temp],$_GET[shidu]%\r\n", FILE_APPEND);
+exit();
 
-function api($url,$field) {
-	$urlmd5=md5($url.$field);
-	$t=filemtime("/tmp/${urlmd5}.txt"); //结果本地缓存半小时
-	if($t<time()+1800) {
-		$str=file_get_contents("/tmp/${urlmd5}.txt");
-	}
-	if($str=='') {
-		$str=file_get_contents($url);
-		$a=json_decode($str,true);
-
-		$str=$a[results][0][$field];
-		file_put_contents("/tmp/${urlmd5}.txt",$str);
-	}
-	return $str;
+function api($url, $field, $fieldx = '')
+{
+ //可以通过其它网站的api获取json格式的数据， 展示到探头上。
+    $urlmd5 = md5($url.$field.$fieldx);
+    $t = filemtime("/tmp/${urlmd5}.txt"); //结果本地缓存半小时
+    if ($t < time() + 1800) {
+        $str = file_get_contents("/tmp/${urlmd5}.txt");
+    }
+    if ($str == '') {
+        $str = file_get_contents($url);
+        $a = json_decode($str, true);
+        if ($fieldx != '') {
+            $str = $a[$field][$fieldx];
+        } else {
+            $str = $a[$field];
+        }
+        $str = round($str, 3);
+        file_put_contents("/tmp/${urlmd5}.txt", $str);
+    }
+    return $str;
 }
