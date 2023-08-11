713cd2980e813430c492f53685715b78c1ffd240
liu shiwei <liushiwei@gmail.com>
2023-08-12 03:03:17 +0800

    升级程序，有时候不到10秒

--- wifi_disp/wifi_disp.ino
+++ wifi_disp/wifi_disp.ino
@@ -46,7 +46,7 @@ void setup()
     get_value();
   get_batt();
   check_batt_low();
-  if (millis() > 10000) { //升级程序后第一次启动
+  if (millis() > 5000) { //升级程序后第一次启动
     Serial.println("升级完成，重启");
     nvram.proc = GENERAL_MODE;
     nvram.nvram7 |= NVRAM7_CHARGE;
