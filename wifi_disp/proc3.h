f52e900590b1e7227ec3b00176fe5b87eeb9841b
liu shiwei <liushiwei@gmail.com>
2023-08-11 20:15:06 +0800

    udp启动次数和millis()之间加个空格

--- wifi_disp/proc3.h
+++ wifi_disp/proc3.h
@@ -6,12 +6,13 @@ void proc3_setup() {
   Serial.println(WiFi.localIP());
   init1();
   disp((char *)"P3  ");
+  set_hostname();
+  hello();
   get_value();
-  Serial.println();
   wait_connected(10000);
   Serial.println(WiFi.localIP());
   if (WiFi_isConnected())
-    udp_send(String(millis()), (char *)"192.168.2.4", 8888, 8888);
+    udp_send(String(nvram.boot_count) + "," + String(millis()), (char *)"192.168.2.4", 8888, 8888);
   nvram.proc = PROC3_MODE;
   system_deep_sleep_set_option(2); //下次开机wifi不校准
   nvram.change = 1;
