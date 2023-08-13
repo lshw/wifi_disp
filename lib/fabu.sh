72f5b2c107d212ab0cb5a080fec380d3c0cdd5c5
liu shiwei <liushiwei@gmail.com>
2023-08-13 21:42:51 +0800

    修改编译脚本，生成2个固件， 第二个有更多的调试信息输出

--- lib/fabu.sh
+++ lib/fabu.sh
@@ -8,8 +8,12 @@ date=`git log --date=short -1 |grep ^Date: |awk '{print $2}' |tr -d '-'`
 ver=$date-${a:0:8}
 echo $ver
 git tag $ver
+export debug=",dbg=Serial,lvl=WIFI"
+lib/build.sh
+mv lib/wifi_disp.bin lib/wifi_disp_dbg.bin
+export debug=",dbg=Disabled,lvl=None____"
 lib/build.sh
 git log >changelog.txt
-cp doc/readme.pdf doc/readme.txt .
-tar cvfz wifi_disp_$ver.tar.gz lib/wifi_disp.bin lib/wifi_disp.php changelog.txt readme.* lib/update.sh lib/*.py
-rm changelog.txt readme.pdf readme.txt
+cp doc/readme.pdf doc/readme.txt  doc/files.* .
+tar cvfz wifi_disp_$ver.tar.gz lib/wifi_disp.bin lib/wifi_disp_dbg.bin lib/wifi_disp.php lib/proc3_socat.sh changelog.txt readme.* files.* lib/update.sh lib/*.py
+rm -f changelog.txt readme.pdf readme.txt files.pdf files.txt
