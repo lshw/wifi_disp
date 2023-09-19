cbd7d2526098a4fe774ae00d41808a4f8d7b6412
liu shiwei <liushiwei@gmail.com>
2023-09-20 07:23:10 +0800

    代码格式调整

--- lib/uncrc32.c
+++ lib/uncrc32.c
@@ -12,7 +12,7 @@ uint32_t crc32c ( uint8_t *message, uint32_t bytes)
   uint32_t mask;
   uint32_t index;
   int8_t shift;
-  uint32_t crc=0xffffffff;
+  uint32_t crc = 0xffffffff;
   for ( index = 0 ; index < bytes ; index++ )
   {
     // Get Next Byte
@@ -62,52 +62,52 @@ void crc32_gen_array( uint32_t crc, int32_t pos, uint8_t *buf, int32_t len )
   }
   return;
 }
-void main(int argc,char * argv[])
+void main(int argc, char * argv[])
 {
   FILE * fp;
   uint32_t crc32;
   char crc32_str[10];
   //  char buf[500000];
-  if(argc!=3) {
-    printf("%s filename crc32_value\r\n",argv[0]);
+  if (argc != 3) {
+    printf("%s filename crc32_value\r\n", argv[0]);
     return;
   }
-  crc32=atol(argv[2]);
-  sprintf(crc32_str,"%ld",crc32);
-  if(strcmp(crc32_str,argv[2])!=0) {
-    printf("crc32_value [%s] is not a number\r\n",argv[2]);
+  crc32 = atol(argv[2]);
+  sprintf(crc32_str, "%ld", crc32);
+  if (strcmp(crc32_str, argv[2]) != 0) {
+    printf("crc32_value [%s] is not a number\r\n", argv[2]);
     return;
   }
-  if(access(argv[1],R_OK)!=0) {
-    printf("file [%s] read error\r\n",argv[1]);
+  if (access(argv[1], R_OK) != 0) {
+    printf("file [%s] read error\r\n", argv[1]);
     return;
   }
   struct stat statbuf;
-  stat(argv[1],&statbuf);
-  uint32_t size=statbuf.st_size;
-  printf("size=%d\r\n",size);
+  stat(argv[1], &statbuf);
+  uint32_t size = statbuf.st_size;
+  printf("size=%d\r\n", size);
   char *buf;
-  buf=malloc(size+4);
-  fp=fopen(argv[1],"a+");
-  if(!fp) {
+  buf = malloc(size + 4);
+  fp = fopen(argv[1], "a+");
+  if (!fp) {
     printf("fopen error\r\n");
     free(buf);
     return;
   }
   uint32_t size0;
-  size0=fread(buf,1,size,fp);
+  size0 = fread(buf, 1, size, fp);
 
-  if(crc32c(buf,size)!=crc32)
+  if (crc32c(buf, size) != crc32)
   {
-    crc32_gen_array (crc32, size, buf, size+4);
-    for (uint32_t i = size; i < size+4; i++)
+    crc32_gen_array (crc32, size, buf, size + 4);
+    for (uint32_t i = size; i < size + 4; i++)
     {
       printf ("%02X", (uint8_t)buf[i]);
     }
     printf(" crc ok\r\n");
   }
-  fseek(fp,0,SEEK_END);
-  fwrite(&buf[size],1,4,fp);
+  fseek(fp, 0, SEEK_END);
+  fwrite(&buf[size], 1, 4, fp);
   fclose(fp);
   free(buf);
   return;
